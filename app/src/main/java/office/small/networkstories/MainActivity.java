3de94ea50bd6a19cda2a3bc7cd08447b87e4edd6
Pavel Korzhenko <pksmall@live.ru>
2018-05-05 12:15:50 +0300

    okHttp with Threads

--- app/src/main/java/office/small/networkstories/MainActivity.java
+++ app/src/main/java/office/small/networkstories/MainActivity.java
@@ -8,14 +8,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.webkit.DownloadListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -26,12 +23,19 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import okhttp3.Headers;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class MainActivity extends AppCompatActivity {
 
     private TextView mInfoTextView;
     private ProgressBar progressBar;
     private EditText editText;
-
+    OkHttpClient client;
+    HttpUrl.Builder urlBuilder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,27 +46,32 @@ public class MainActivity extends AppCompatActivity {
         mInfoTextView = findViewById(R.id.tvLoad);
         progressBar = findViewById(R.id.progressBar);
         Button btnLoad = findViewById(R.id.btnLoad);
+        client = new OkHttpClient();
         btnLoad.setOnClickListener((v) -> onClick());
     }
 
     private void onClick() {
-        String bastUrl = "https://api.github.com/users";
+        urlBuilder = HttpUrl.parse("https://api.github.com/users").newBuilder();
         if (!editText.getText().toString().isEmpty()) {
-            bastUrl += "/" + editText.getText();
+            urlBuilder.addEncodedPathSegment(editText.getText().toString()); // one user
         }
+
+        String url = urlBuilder.build().toString();
+        Request request = new Request.Builder().url(url).build();
 
         ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
 
         if (networkInfo != null && networkInfo.isConnected()) {
-            new DownloadPageTask().execute(bastUrl);
+            new DownloadPageTask().execute(request);
         } else {
             Toast.makeText(this, "Network is off. Turn it on.", Toast.LENGTH_SHORT).show();
         }
     }
 
-    private class DownloadPageTask extends AsyncTask<String, Void, String> {
+    private class DownloadPageTask extends AsyncTask<Request, Void, String> {
         private static final String DONEURL = "DOWNONEURL";
+        Response response;
 
         @Override
         protected void onPreExecute() {
@@ -79,7 +88,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @Override
-        protected String doInBackground(String... urls) {
+        protected String doInBackground(Request... urls) {
             try {
                 return downloadOneUrl(urls[0]);
             } catch (IOException e) {
@@ -88,60 +97,40 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
-        private String downloadOneUrl(String sUrl) throws IOException {
-            InputStream inputStream = null;
+        private String downloadOneUrl(Request sUrl) throws IOException {
             String data = "";
 
+
             try {
-                URL url = new URL(sUrl);
-                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-                connection.setReadTimeout(10000);
-                connection.setConnectTimeout(10000);
-                connection.setInstanceFollowRedirects(true);
-                connection.setUseCaches(false);
-                connection.setDoInput(true);
 
-                int responseCode = connection.getResponseCode();
-                if (responseCode == HttpURLConnection.HTTP_OK) {
-                    Log.d(DONEURL, "REQ METHOD: " + connection.getRequestMethod());
-                    Log.d(DONEURL, "RCV MSG: " + connection.getResponseMessage());
-
-                    Map<String, List<String>> myMap = connection.getHeaderFields();
-                    Set<String> myFields = myMap.keySet();
-                    Log.d(DONEURL, "HEADERS");
-                    for(String k: myFields) {
-                        Log.d(DONEURL, "Key: " + k + " Values: " + myMap.get(k));
-                    }
-                    inputStream = connection.getInputStream();
-                    ByteArrayOutputStream bos = new ByteArrayOutputStream();
-                    int read;
-                    while((read = inputStream.read()) != 1) {
+                final Thread thread = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
                         try {
-                            bos.write(read);
-                        } catch (OutOfMemoryError e) {
+                            response = client.newCall(sUrl).execute();
+                        } catch (IOException e) {
                             e.printStackTrace();
-                            data = new String("Error. Out of memory");
-                            break;
-                        }
-                        if (bos.size() > 100000) {
-                            break;
                         }
                     }
-                    byte[] result = bos.toByteArray();
-                    bos.close();
-                    data = new String(result);
+                });
+
+                thread.start();
+                thread.join();
+
+                if (!response.isSuccessful()) {
+                    throw new IOException("Unexpected code" + response);
                 } else {
-                    data = connection.getResponseMessage() + ". Error Code: " + responseCode;
-                 }
-                 connection.disconnect();
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
+                    Headers responseHeaders = response.headers();
+                    Log.d(DONEURL, "HEADERS");
+                    for(int i = 0; i < responseHeaders.size(); i++) {
+                        Log.d(DONEURL, "Key: " + responseHeaders.name(i) + " Values: " + responseHeaders.value(i));
+                    }
+                    data = response.body().string();
+                }
             } catch (IOException e) {
                 e.printStackTrace();
-            } finally {
-                if (inputStream != null) {
-                    inputStream.close();
-                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
             return  data;
         }
